a7cff2aefaff5c542ad774132781c04e48d14133
Daniel <daniel.figg@gmail.com>
2023-10-25 23:24:38 -0400

    shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-import { api } from "~/trpc/server";
-
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-
   return (
     <main className="flex ">
       <UserButton />
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-    </div>
   );
 }
