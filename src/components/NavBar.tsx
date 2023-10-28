b3a7b1c970c7edce9b1689784188247a0f3d7ab6
daniel-figg <daniel.figg05@gmail.com>
2023-10-27 23:19:19 -0400

    refactor to investments and balance

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,56 +1,5 @@
 "use client";
 
-// import Link from "next/link";
-// import {
-//   NavigationMenu,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuList,
-//   navigationMenuTriggerStyle,
-// } from "./ui/navigation-menu";
-// import { ModeToggle } from "./ui/mode-toggle";
-// import { UserButton } from "@clerk/nextjs";
-
-// const NavBar = () => {
-//   return (
-//     <NavigationMenu className="container m-0 flex h-14 min-w-full items-center justify-between border-b">
-//       <NavigationMenuList className="m-4 gap-4">
-//         <NavigationMenuItem>
-//           <Link href="/" legacyBehavior passHref>
-//             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-//               <h1 className="text-lg font-bold">Finance</h1>
-//             </NavigationMenuLink>
-//           </Link>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <Link href="/transactions" legacyBehavior passHref>
-//             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-//               <h1 className="text-muted-foreground">Transactions</h1>
-//             </NavigationMenuLink>
-//           </Link>
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <Link href="/portfolio" legacyBehavior passHref>
-//             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-//               <h1 className="text-muted-foreground">Portfolio</h1>
-//             </NavigationMenuLink>
-//           </Link>
-//         </NavigationMenuItem>
-//       </NavigationMenuList>
-//       <NavigationMenuList className="m-4 gap-4">
-//         <NavigationMenuItem>
-//           <ModeToggle />
-//         </NavigationMenuItem>
-//         <NavigationMenuItem>
-//           <UserButton />
-//         </NavigationMenuItem>
-//       </NavigationMenuList>
-//     </NavigationMenu>
-//   );
-// };
-
-// export default NavBar;
-
 import React from "react";
 import {
   Navbar,
@@ -72,7 +21,7 @@ export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const pathname = usePathname();
 
-  const menuItems = ["Home", "Portfolio", "Transactions", "Accounts"];
+  const menuItems = ["Home", "Balance", "Transactions", "Investments"];
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} isBordered={true}>
@@ -90,14 +39,14 @@ export default function App() {
       </NavbarContent>
 
       <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-        <NavbarItem isActive={pathname === "/portfolio" ? true : false}>
-          <Link href="/portfolio">Portfolio</Link>
+        <NavbarItem isActive={pathname === "/balance" ? true : false}>
+          <Link href="/balance">Balance</Link>
         </NavbarItem>
         <NavbarItem isActive={pathname === "/transactions" ? true : false}>
           <Link href="/transactions">Transactions</Link>
         </NavbarItem>
-        <NavbarItem isActive={pathname === "/accounts" ? true : false}>
-          <Link href="/accounts">Accounts</Link>
+        <NavbarItem isActive={pathname === "/investments" ? true : false}>
+          <Link href="/investments">Investments</Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
