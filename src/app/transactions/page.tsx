83ec34e18fab878c4e29594ad6ff77851dd36287
daniel-figg <daniel.figg05@gmail.com>
2023-10-27 20:22:39 -0400

    setup new transaction data table

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,12 +1,29 @@
 import DoughnutChart from "~/components/DoughnutChart";
 import HorizontalBarChart from "~/components/HorizontalBarChart";
-import { TransactionTable } from "~/components/TransactionTable";
+// import { TransactionTable } from "~/components/TransactionTable";
+import { type Payment, columns } from "./columns";
+import { DataTable } from "~/components/ui/data-table";
 import VerticalBarChart from "~/components/VerticalBarChart";
 
+function getTransactionData(): Payment[] {
+  // Fetch data from your API here.
+  const fake: Payment[] = [];
+  for (let i = 1; i <= 100; i++) {
+    fake.push({
+      id: i.toString(),
+      amount: i,
+      status: "pending",
+      email: "m@example.com",
+    });
+  }
+  return fake;
+}
+
 const Transactions = () => {
+  const data = getTransactionData();
   return (
     <div className="flex flex-col justify-center">
-      <div className="mx-4 flex max-h-80 max-w-full flex-row">
+      <div className="mx-auto flex max-h-80 max-w-full flex-row">
         <div className="flex-1">
           <DoughnutChart />
         </div>
@@ -17,7 +34,9 @@ const Transactions = () => {
           <HorizontalBarChart />
         </div>
       </div>
-      <TransactionTable />
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 };
