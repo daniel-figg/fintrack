6f1b321ce0e66889dfd8d182e5a288fa11c441f7
daniel-figg <daniel.figg05@gmail.com>
2023-10-28 22:30:43 -0400

    fix border

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex items-center justify-center border">
+    <div className="flex items-center justify-center">
       <SignIn
         appearance={{
           elements: {
