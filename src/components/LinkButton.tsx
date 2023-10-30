0c85c7e05846bbbba01c013848a3b61a26d236bb
daniel-figg <daniel.figg05@gmail.com>
2023-10-30 11:43:22 -0400

    linting

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -12,13 +12,14 @@ const LinkButton: React.FC<linkButton> = ({ linkToken }) => {
   const { open, ready } = usePlaidLink<PlaidLinkOptions>({
     token: linkToken,
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    onSuccess: async (public_token, metadata): Promise<undefined> => {
+    onSuccess: async (public_token, _metadata): Promise<undefined> => {
       // send public_token to server
       await Exchange(public_token);
     },
   });
 
   return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     <Button onClick={() => open()} disabled={!ready} variant="default">
       Connect a bank account
     </Button>
