bb2e819f7a1261f330627cc0b7705735d4ee4c4a
daniel-figg <daniel.figg05@gmail.com>
2023-10-30 22:35:34 -0400

    temporary transaction calls from link router

--- src/server/api/routers/link.ts
+++ src/server/api/routers/link.ts
@@ -9,6 +9,9 @@ import {
   PlaidEnvironments,
   Products,
   CountryCode,
+  Transaction,
+  RemovedTransaction,
+  TransactionsSyncRequest,
 } from "plaid";
 
 const configuration = new Configuration({
@@ -20,7 +23,7 @@ const configuration = new Configuration({
     },
   },
 });
-const plaidClient = new PlaidApi(configuration);
+export const plaidClient = new PlaidApi(configuration);
 
 export const linkRouter = createTRPCRouter({
   createLinkToken: publicProcedure.query(async () => {
@@ -43,25 +46,141 @@ export const linkRouter = createTRPCRouter({
     }
   }),
 
-  exchangePublicToken: publicProcedure
-    .input(z.string())
-    .mutation(async ({ ctx, input }) => {
-      const request: ItemPublicTokenExchangeRequest = {
-        public_token: input,
-      };
+  sync: publicProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
+    // Provide a cursor from your database if you've previously
+    // received one for the Item. Leave null if this is your
+    // first sync call for this Item. The first request will
+    // return a cursor.
+    //   let cursor = database.getLatestCursorOrNull(itemId);
+    const { accessToken, itemId } = await ctx.db.items.findFirstOrThrow({
+      where: {
+        userId: input,
+      },
+      select: {
+        accessToken: true,
+        itemId: true,
+      },
+    });
 
-      try {
-        const response = await plaidClient.itemPublicTokenExchange(request);
-        const generatedToken = response.data.access_token;
-        const generatedId = response.data.item_id;
-        await ctx.db.user.create({
-          data: {
-            accessToken: generatedToken,
-            itemId: generatedId,
-          },
-        });
-      } catch (err) {
-        // handle error
-      }
-    }),
+    const { transactionCursor } = await ctx.db.items.findFirstOrThrow({
+      where: {
+        itemId: itemId,
+      },
+      select: {
+        transactionCursor: true,
+      },
+    });
+
+    // New transaction updates since "cursor"
+    let added: Array<Transaction> = [];
+    let modified: Array<Transaction> = [];
+    // Removed transaction ids
+    let removed: Array<RemovedTransaction> = [];
+    let hasMore = true;
+    let cursor = transactionCursor;
+
+    // Iterate through each page of new transaction updates for item
+    while (hasMore) {
+      const request: TransactionsSyncRequest = {
+        access_token: accessToken,
+        cursor: cursor,
+      };
+      const response = await plaidClient.transactionsSync(request);
+      const data = response.data;
+      console.log(data);
+
+      // Add this page of results
+      added = added.concat(data.added);
+      modified = modified.concat(data.modified);
+      removed = removed.concat(data.removed);
+
+      hasMore = data.has_more;
+
+      // Update cursor to the next cursor
+      cursor = data.next_cursor;
+    }
+
+    // Persist cursor and updated data
+    added.map(async (transaction) => {
+      await ctx.db.transactions.create({
+        data: {
+          transactionId: transaction.transaction_id,
+          userId: input,
+          accountId: transaction.account_id,
+          category: transaction.personal_finance_category?.primary,
+          date: transaction.authorized_date ?? transaction.date,
+          name: transaction.merchant_name ?? transaction.name,
+          amount: transaction.amount,
+          currencyCode: transaction.iso_currency_code ?? "USD",
+          confidenceLevel:
+            transaction.personal_finance_category?.confidence_level,
+        },
+      });
+    });
+
+    modified.map(async (transaction) => {
+      await ctx.db.transactions.update({
+        where: {
+          transactionId: transaction.transaction_id,
+        },
+        data: {
+          transactionId: transaction.transaction_id,
+          accountId: transaction.account_id,
+          category: transaction.personal_finance_category?.primary,
+          date: transaction.authorized_date ?? transaction.date,
+          name: transaction.merchant_name ?? transaction.name,
+          amount: transaction.amount,
+          currencyCode: transaction.iso_currency_code ?? "USD",
+          confidenceLevel:
+            transaction.personal_finance_category?.confidence_level,
+        },
+      });
+    });
+
+    removed.map(async (transaction) => {
+      await ctx.db.transactions.delete({
+        where: {
+          transactionId: transaction.transaction_id,
+        },
+      });
+    });
+
+    await ctx.db.items.update({
+      where: {
+        itemId: itemId,
+      },
+      data: {
+        transactionCursor: cursor,
+      },
+    });
+  }),
+  getData: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
+    return await ctx.db.transactions.findMany({
+      where: {
+        userId: input,
+      },
+    });
+  }),
+
+  // exchangePublicToken: publicProcedure
+  //   .input(z.string())
+  //   .mutation(async ({ ctx, input }) => {
+  //     const request: ItemPublicTokenExchangeRequest = {
+  //       public_token: input,
+  //     };
+
+  //     try {
+  //       const response = await plaidClient.itemPublicTokenExchange(request);
+  //       const generatedToken = response.data.access_token;
+  //       const generatedId = response.data.item_id;
+  //       await ctx.db.user.create({
+  //         data: {
+  //           accessToken: generatedToken,
+  //           itemId: generatedId,
+  //         },
+  //       });
+  //     } catch (err) {
+  //       // handle error
+  //     }
+  //   }),
 });
