1dc36fa617952ee2091c589cbe4672c3f3a9f052
Daniel <daniel.figg@gmail.com>
2023-11-08 15:33:33 -0500

    add investment & balance router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,8 @@ import { createTRPCRouter } from "~/server/api/trpc";
 import { linkRouter } from "./routers/link";
 import { transactionRouter } from "./routers/transaction";
 import { accountRouter } from "./routers/account";
+import { investmentRouter } from "./routers/investment";
+import { balanceRouter } from "./routers/balance";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +14,8 @@ export const appRouter = createTRPCRouter({
   link: linkRouter,
   transaction: transactionRouter,
   account: accountRouter,
+  investment: investmentRouter,
+  balance: balanceRouter,
 });
 
 // export type definition of API
